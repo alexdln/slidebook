f98f9c2cd4e7ab61add9a01186b3353a0047fe24
Vordgi <sasha2822222@gmail.com>
2025-07-22 22:04:48 +0100

    sb-175 fix ejected app check

--- packages/cli/lib/constants.js
+++ packages/cli/lib/constants.js
@@ -5,9 +5,9 @@ import fs from "fs";
 export const IS_LIMITED_CI = Boolean(process.env.VERCEL_PROJECT_ID) || process.argv[2] === "eject";
 
 export const IS_EJECTED =
-    fs.existsSync(path.join(process.cwd(), "next.fs.js")) ||
-    fs.existsSync(path.join(process.cwd(), "next.fs.mjs")) ||
-    fs.existsSync(path.join(process.cwd(), "next.fs.ts"));
+    fs.existsSync(path.join(process.cwd(), "next.config.js")) ||
+    fs.existsSync(path.join(process.cwd(), "next.config.mjs")) ||
+    fs.existsSync(path.join(process.cwd(), "next.config.ts"));
 
 export const OUT_DIR = IS_LIMITED_CI || IS_EJECTED ? process.cwd() : path.join(process.cwd(), "build", ".tmp");
 
